6b2f6e927517e21fd43cdd4856341231380e6989
Bruno Tarditi <brunotarditi@hotmail.com>
2020-08-17 20:17:34 -0300

    ejercicio 1 resuelto

---
+++ Trabajo-Práctico-1/Ejercicio-1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 5. 
+ * Utiliza las iteraciones for, while, do/while para resolver el mismo problema.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine("El número " + i + " es divisible por 2 y 5.");
+                }
+            }
+        }
+    }
+}
