7aa341d1240ab8ec87b305ebab109d14e421b1fc
Bruno Tarditi <brunotarditi@hotmail.com>
2020-09-04 15:35:38 -0300

    tp2 finalizado

--- Trabajo-Práctico-2/D7/ejercicio-7.cs
+++ Trabajo-Práctico-2/D7/ejercicio-7.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*En primer lugar, cree una clase base llamada Figura y luego cree clases derivadas como Rectángulo, Circulo y Triangulo.
+Incluya en la clase Figura un método virtual llamado Dibujar() que imprima por consola el mensaje “Dibuja Figura” y 
+sobrescriba el método (override) en cada clase derivada para dibujar la forma determinada que representa la clase, es decir 
+que para Rectángulo el método debe imprimir por consola “Dibuja Rectángulo”, para Circulo “Dibuja Circulo” y para Triangulo 
+“Dibuja Triangulo”.
+Finalmente en el método static void Main() cree un objeto List<Figura> y agregue al mismo instancias de Circulo, Triangulo y 
+Rectángulo.
+Utilice un bucle foreach para recorrer en iteración la lista y llamar al método Dibujar () en cada objeto Figura de la lista.
+Aunque cada objeto de la lista tiene un tipo declarado de Figura, es el tipo en tiempo de ejecución el que se invocará.
+El resultado del programa debería ser:
+Dibuja Rectángulo
+Dibuja Figura
+Dibuja Círculo
+Dibuja Figura
+Dibuja Triangulo
+Dibuja Figura
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Trabajo-Práctico-2/E1/ejercicio-8.cs
+++ Trabajo-Práctico-2/E1/ejercicio-8.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*Codifique un programa que contenga un método llamado “ejecutarHiloCincoSeg()” que imprima por consola la leyenda 
+ * “HILO EJECUTADO 5” y otro método llamado “ejecutarHiloDiezSeg()”que imprima por consola la leyenda “HILO EJECUTADO 10” 
+ * implemente el código necesario para que por medio de la ejecución de Hilos el mensaje del método “ejecutarHiloCincoSeg()” 
+ * se ejecute y muestre cada 5 segundos y el mensaje del método “ejecutarHiloDiezSeg()” se ejecute y muestre cada 10 segundos.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
