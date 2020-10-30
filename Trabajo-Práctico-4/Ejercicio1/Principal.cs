009c9f5238fd0ac5e845cd80d0465442a108e1ec
Bruno Tarditi <brunotarditi@hotmail.com>
2020-10-30 20:16:58 -0300

    Se añade la consigna

--- Trabajo-Práctico-4/Ejercicio1/Principal.cs
+++ Trabajo-Práctico-4/Ejercicio1/Principal.cs
@@ -7,6 +7,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
+/*Realice una conexión a la base de datos utn, seleccionaremos los datos de la tabla artículos 
+ * haciendo uso de la cláusula LIMIT, ejemplo
+SELECT * FROM articulo LIMIT 0, 50; //selecciona los registros del 1 al 50
+SELECT * FROM articulo LIMIT 50, 100; //selecciona los registros del 51 al 100
+ Escritura del Archivo: Mediante el uso de la clase StreamWriter realice la escritura de un archivo llamado 
+articulos.txt que contenga la totalidad de los campos de la tabla artículos. La primera fila del archivo debe 
+corresponder al encabezado y debe contener el nombre de las columnas de la tabla.
+La escritura del archivo debe hacerse mediante grupos de 50 registros, objetivo que logramos aplicando la 
+cláusula LIMIT descripta anteriormente.
+Realice la separación de los campos mediante tabulaciones “\t” y la separación de cada fila mediante saltos 
+de línea “\n”. Verifique que el archivo pueda abrirse haciendo uso del programa EXCEL.
+ */
+
 namespace Ejercicio1
 {
     class Principal
--- Trabajo-Práctico-4/Ejercicio2/Principal.cs
+++ Trabajo-Práctico-4/Ejercicio2/Principal.cs
@@ -5,7 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
-
+/*
+ Lectura del Archivo: Mediante el uso de la clase StreamReader realice la lectura del archivo articulos.txt 
+generado en el punto anterior, posteriormente realice una conexión a la base de datos utn e inserte en la tabla 
+articulo_copy la información leída del archivo. La inserción en la tabla de base de datos debe realizarse 
+ejecutando 1 transacción cada 50 filas leídas del archivo, deberá determinar si corresponde realizar un INSERT o 
+un UPDATE haciendo uso del campo código para determinar si el registro ya existe o no en la tabla. Al finalizar 
+controle que la cantidad de registros de la tabla articulo y articulo_copy sean la misma.
+ */
 namespace Ejercicio2
 {
     class Principal 
